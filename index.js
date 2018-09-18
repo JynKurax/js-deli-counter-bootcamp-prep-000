56e86b7cc25b8fa689de2614cea69b3424eeacca
Sabu <sabuw319@gmail.com>
2018-09-18 15:08:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function takeANumber(cusName, lineNum){
+  katzDeliLine.push(cusName)
+  return `Greetings, ${cusname}. You are number .`
+}
+
+function nowServing(katzDeliLine){
+  if(cusName.length > 0){
+    return cusName[0]
+    delete cusName[0]
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine){
+  var i = 0
+  while(i < katzDeliLine.length){
+   return `The line is currently: 
+ }
+}
